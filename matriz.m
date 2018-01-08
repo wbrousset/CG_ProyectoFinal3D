39b373d70dadc053b27e2b985861f1e9807b8827
wbrousset <wbrousset@gmail.com>
2018-01-08 14:14:36 -0500

    Add files via upload

---
+++ matriz.m
@@ -0,0 +1,21 @@
+function[P] = matriz(a)
+K = a.KK;
+x = size(a.ind_active)
+Rt = struct([])
+Rt(1) = a.Rc_1
+Rt(2) = a.Rc_2
+Rt(3) = a.Rc_3
+Rt(4) = a.Rc_4
+Rt(5) = a.Rc_5
+Rt(6) = a.Rc_6
+Rt(7) = a.Rc_7
+Rt(8) = a.Rc_8
+Rt(9) = a.Rc_9
+Rt(10) = a.Rc_10
+Rt(11) = a.Rc_11
+Rt(12) = a.Rc_12
+Rt(13) = a.Rc_13
+Rt(14) = a.Rc_14
+Rt(15) = a.Rc_15
+Rt(16) = a.Rc_16
+end
