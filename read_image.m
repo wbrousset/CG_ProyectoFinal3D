a2b1a08e0e902c617b1f8315620ca707a1a48bba
wbrousset <wbrousset@gmail.com>
2018-01-08 14:07:44 -0500

    Delete read_image.m

--- read_image.m
+++
@@ -1,7 +0,0 @@
-function image = read_image(imagenumber)
-
-imageFileNumber = sprintf('%.2d', imagenumber);
-imageFile = strcat('data/images/', imageFileNumber, '.jpg');
-image = imread(imageFile);
-
-return
